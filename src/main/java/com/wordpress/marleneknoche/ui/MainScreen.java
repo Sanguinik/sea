1384454d5b1c45389332d56538f80865a1b36459
Marlene <Knoche.Marlene@gmx.de>
2013-05-07 16:06:32 +0200

    Added JavaFX dependencies for project.

---
+++ src/main/java/com/wordpress/marleneknoche/ui/MainScreen.java
@@ -0,0 +1,29 @@
+package com.wordpress.marleneknoche.ui;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class MainScreen extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+		primaryStage.setTitle("sea - dna sequence analyzer");
+		
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		
+		Scene scene = new Scene(grid, 400,200);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
