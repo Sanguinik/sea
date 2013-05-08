814c0ec373dd03ff0ac6e435470c446586473da0
Marlene <Knoche.Marlene@gmx.de>
2013-05-08 13:46:57 +0200

    new test class: nucleobase counter

---
+++ src/main/java/com/wordpress/marleneknoche/sea/logic/NucleobaseCounter.java
@@ -0,0 +1,23 @@
+package com.wordpress.marleneknoche.sea.logic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NucleobaseCounter {
+
+	public HashMap<String, Integer> countNucleobases(String testString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int countPurines(Map<String, Integer> nucleobaseMap) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int countPyrimidines(Map<String, Integer> nucleobaseMap) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/test/java/com/wordpress/marleneknoche/sea/logic/NucleobaseCounterTest.java
@@ -0,0 +1,70 @@
+package com.wordpress.marleneknoche.sea.logic;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NucleobaseCounterTest {
+
+	private NucleobaseCounter nucleobaseCounter;
+	private static final String TEST_STRING = "GTACGCCTGGGAGTCGACTGACTGCTGAAGATACAGAACCTGA";
+	
+	@Before
+	public void setUp(){
+		nucleobaseCounter = new NucleobaseCounter();
+	}
+
+	@Test
+	public void countNucleobasesTest() {
+		
+		Map<String,Integer> nucleobaseMap = new HashMap<String,Integer>();
+		
+		nucleobaseMap = nucleobaseCounter.countNucleobases(TEST_STRING);
+		
+		assertEquals("13", nucleobaseMap.get("G"));
+		assertEquals("8", nucleobaseMap.get("T"));
+		assertEquals("10", nucleobaseMap.get("C"));
+		assertEquals("12", nucleobaseMap.get("A"));
+	}
+	
+	@Test
+	public void countPurinesTest(){
+		
+		//A+G=pu
+		
+		Map<String,Integer> nucleobaseMap = new HashMap<String,Integer>();
+		
+		nucleobaseMap.put("G", 13);
+		nucleobaseMap.put("T", 8);
+		nucleobaseMap.put("C", 10);
+		nucleobaseMap.put("A", 12);
+		
+		int numberOfPurines = nucleobaseCounter.countPurines(nucleobaseMap);
+		
+		assertEquals(25, numberOfPurines);
+		
+	}
+	
+	@Test
+	public void countPyrimidinesTest(){
+		
+		//C+T=py
+		
+		Map<String,Integer> nucleobaseMap = new HashMap<String,Integer>();
+		
+		nucleobaseMap.put("G", 13);
+		nucleobaseMap.put("T", 8);
+		nucleobaseMap.put("C", 10);
+		nucleobaseMap.put("A", 12);
+		
+		int numberOfPyrimidines = nucleobaseCounter.countPyrimidines(nucleobaseMap);
+		
+		assertEquals(18, numberOfPyrimidines);
+		
+	}
+
+}
