5f10301d0c81f9e35f008e866b4d43b2ac7565da
Marlene <Knoche.Marlene@gmx.de>
2013-05-10 23:48:43 +0200

    Built UI

--- src/main/java/com/wordpress/marleneknoche/sea/ui/MainScreen.java
+++ src/main/java/com/wordpress/marleneknoche/sea/ui/MainScreen.java
@@ -1,22 +1,23 @@
 package com.wordpress.marleneknoche.sea.ui;
 
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class MainScreen extends Application {
 
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws IOException {
 		
 		primaryStage.setTitle("sea - dna sequence analyzer");
 		
-		GridPane grid = new GridPane();
-		grid.setAlignment(Pos.CENTER);
+		AnchorPane root = FXMLLoader.load(getClass().getResource("FileLoaderScreen.fxml"));
 		
-		Scene scene = new Scene(grid, 400,200);
+		Scene scene = new Scene(root, 600,400);
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
